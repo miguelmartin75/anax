2eb3839b4f631d6aa7d000bc35791fe54b8e3fde
Miguel Martin <miguel.martin7.5@hotmail.com>
2013-12-26 18:32:20 +1030

    Updated version number to 1.1

--- include/anax/anax.hpp
+++ include/anax/anax.hpp
@@ -48,7 +48,7 @@ namespace anax
 /// <a href="https://github.com/miguelishawt/anax/wiki"> here</a>.
 
 #define ANAX_VERSION_MAJOR 1
-#define ANAX_VERSION_MINOR 0
+#define ANAX_VERSION_MINOR 1
 #define ANAX_PATCH_NUMBER 0
 
 #define ANAX_VERSION_NUMBER ANAX_VERSION_MAJOR.ANAX_VERSION_MINOR.ANAX_PATCH_NUMBER
