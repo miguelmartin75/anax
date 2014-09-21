79ccb650347d00e7f2441c16a9a3f52ca3c28f0d
Miguel Martin <miguel@miguel-martin.com>
2014-09-22 03:39:50 +0930

    altered install script to be much nicer now can take in an agument for where the cmake config is

--- install.sh
+++ install.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
-pushd build
 
-# Debug Builds
-# note: 
-# only need to install headers once, and may as well do it whilst
-# compiling/installing one binary
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DINSTALL_HEADERS=ON
-sudo make install
+if [ $# -ge 2 ]; then
+    CMAKE_PATH=$1
+else
+    CMAKE_PATH=build
+fi
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DINSTALL_HEADERS=OFF
-sudo make install
+echo "using $CMAKE_PATH for cmake config"
 
-# Release Builds
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DINSTALL_HEADERS=OFF
-sudo make install
+if [ ! -d $CMAKE_PATH ]; then
+    echo "${CMAKE_PATH} does not exist!"
+    if [ $ARGC >= 1 ]; then
+        echo "please ensure you did not mis-spell your directory path"
+    else
+        echo "you may provide one argument to the this script;"
+        echo "the directory where the cmake files are generated"
+    fi
+fi
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DINSTALL_HEADERS=OFF
-sudo make install
+
+OLD_PATH=$PWD
+
+pushd $CMAKE_PATH
+
+BUILD_TYPES=(Debug Release)
+ON_OFF=(ON OFF)
+
+i=0
+for buildType in $BUILD_TYPES
+do
+    for sharedLibs in $ON_OFF
+    do
+        INSTALL_HEADERS=OFF
+        if [ i != 0 ]; then
+            INSTALL_HEADERS=ON
+        fi
+
+        cmake $OLD_PATH -DCMAKE_BUILD_TYPE=$buildType -DBUILD_SHARED_LIBS=$sharedLibs -DINSTALL_HEADERS=OFF
+        make install
+
+        i=i+1
+    done
+done
 
 popd
