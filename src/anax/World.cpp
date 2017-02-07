2426af3ce1346da31a1d8bba52f9bd750d2f9468
sonicx <ninnghazad@dnophos.de>
2017-02-07 12:26:36 +0100

    Added isRefreshing() to World and some asserts to forbid calling activate,deactivate and killEntity while World is refreshing.

--- src/anax/World.cpp
+++ src/anax/World.cpp
@@ -83,6 +83,8 @@ namespace anax
 
     void World::killEntity(Entity& entity)
     {
+        ANAX_ASSERT(!isRefreshing(), "cannot kill entities while refreshing");
+
         // deactivate the entity
         deactivateEntity(entity);
 
@@ -101,6 +103,7 @@ namespace anax
     void World::activateEntity(Entity& entity)
     {
         ANAX_ASSERT(isValid(entity), "invalid entity tried to be activated");
+        ANAX_ASSERT(!isRefreshing(), "cannot activate entities while refreshing");
 
         m_entityCache.activated.push_back(entity);
     }
@@ -108,6 +111,7 @@ namespace anax
     void World::deactivateEntity(Entity& entity)
     {
         ANAX_ASSERT(isValid(entity), "invalid entity tried to be deactivated");
+        ANAX_ASSERT(!isRefreshing(), "cannot deactivate entities while refreshing");
 
         m_entityCache.deactivated.push_back(entity);
     }
@@ -125,8 +129,15 @@ namespace anax
         return m_entityIdPool.isValid(entity.getId());
     }
 
+    bool World::isRefreshing() const
+    {
+        return m_refreshing;
+    }
+
     void World::refresh()
     {
+	m_refreshing = true;
+
         // go through all the activated entities from last call to refresh
         for(auto& entity : m_entityCache.activated)
         {
@@ -198,6 +209,8 @@ namespace anax
 
         // clear the temp cache
         m_entityCache.clearTemp();
+
+	m_refreshing = false;
     }
 
     void World::clear()
